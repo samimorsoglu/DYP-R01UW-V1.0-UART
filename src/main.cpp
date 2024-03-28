9bb2c1a5e06f6afa29239ee1a0f22b5966ff6bb7
Samim Örsoğlu <samimorsoglu@gmail.com>
2024-03-28 16:28:21 +0300

    Byte analysis has been conducted.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
-#include <Arduino.h>
+#include <HardwareSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define RX_PIN 16 // Sensörün TX pininin bağlı olduğu ESP32 pin numarası 
+#define TX_PIN 17 // Sensörün RX pininin bağlı olduğu ESP32 pin numarası 
+
+HardwareSerial sensorSerial(1); 
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); 
+  sensorSerial.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (sensorSerial.available() >= 4) { 
+    uint8_t data[4];
+    for (int i = 0; i < 4; i++) {
+      data[i] = sensorSerial.read();
+    }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    uint8_t frame = data[0];
+    uint8_t data_h = data[1];
+    uint8_t data_l = data[2];
+    uint8_t sum = data[3];
+
+    Serial.print("Frame: "); Serial.println(frame);
+    Serial.print("Data_H: "); Serial.println(data_h);
+    Serial.print("Data_L: "); Serial.println(data_l);
+    Serial.print("Sum: "); Serial.println(sum);
+  }
+  
+  delay(100); 
 }
